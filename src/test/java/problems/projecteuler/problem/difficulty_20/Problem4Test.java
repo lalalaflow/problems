b51bf0385f397dda4a9fdbcdcf2289d035fef014
Sujeong Yeom <lalalaflow@gmail.com>
2017-01-22 19:23:28 +0900

    Create Test file for Problem4

---
+++ src/test/java/problems/projecteuler/problem/difficulty_20/Problem4Test.java
@@ -0,0 +1,22 @@
+package problems.projecteuler.problem.difficulty_20;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by yarbong on 22/01/2017.
+ */
+public class Problem4Test {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void solve() throws Exception {
+
+    }
+
+}
