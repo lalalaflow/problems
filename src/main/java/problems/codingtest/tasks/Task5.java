e76ccecf43f1d3e099c6f48382c86729a43403ea
Sujeong Yeom <lalalaflow@gmail.com>
2017-01-26 22:40:09 +0900

    Refactoring (readability)

--- src/main/java/problems/codingtest/tasks/Task5.java
+++ src/main/java/problems/codingtest/tasks/Task5.java
@@ -65,10 +65,12 @@ public class Task5 {
         //Comparator 가 rating value가 되도록
         TreeSet<Song> tSimilarMovies = new TreeSet<>(Comparator.comparing(Song::getRating));
         tSimilarMovies.addAll(aSimilarMovies);
+
         Predicate<Song> moviePredicate =
                 recommMovie -> getSelectedSong().getId() == recommMovie.getId();
         //추천을 요청한 노드와의 중복 제거, 기본 비교자가 Rating 이므로 아이디로 구별하여 제거하도록 Predicate정의
         tSimilarMovies.removeIf(moviePredicate) ;
+
         return tSimilarMovies.descendingSet();
     }
 
